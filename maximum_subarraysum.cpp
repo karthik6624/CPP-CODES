8672cc6243e7c0db675f1e4a17740fa5ac15fc30
Karthik Kandi <101356427+karthik6624@users.noreply.github.com>
2023-03-30 01:05:18 +0530

    Create maximum_subarraysum.cpp

---
+++ maximum_subarraysum.cpp
@@ -0,0 +1,10 @@
+long long maximumsubarraysum(int arr[],int n){
+    long long sum=0,maxi=LONG_MIN;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        if(sum>maxi)maxi=sum;
+        if(sum<0)sum=0;
+    }
+    if(maxi<0)maxi=0;
+    return maxi;
+}
